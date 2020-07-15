6a9596f93c2ecb85f01fd9fb490638745f02aec3
Karl Beecken <karl@beecken.berlin>
2020-07-15 21:13:33 +0200

    🧹 clean up

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json({message: 'hello world'});
 });
 
 module.exports = router;
