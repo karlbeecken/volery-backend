0dbee8f97648616e8f4a23361614b63362cedb8d
Karl Beecken <karl@beecken.berlin>
2020-07-16 22:20:08 +0200

    💾 add basic requests

--- app.js
+++ app.js
@@ -4,24 +4,28 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const usersRouter = require("./routes/users");
+const tweetsRouter = require("./routes/tweets");
 
 let app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/users", usersRouter);
+app.use("/tweets?", tweetsRouter);
 
 const mongoDB = "mongodb://localhost/tweets";
+mongoose.set("useFindAndModify", false);
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 let db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 Tweet = require("./tweetModel");
-let tweetInstance = new Tweet({ name: "testtweet" });
-tweetInstance.save(function (err) {
-  if (err) return handleError(err);
-});
+// let tweetInstance = new Tweet({ name: "testtweet" });
+// Tweet.find({}, (err, docs) => {
+//   docs.forEach(doc => doc.deleteOne());
+// })
 
 module.exports = app;
---
+++ routes/tweets.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const bodyParser = require("body-parser");
+const Tweet = require("../tweetModel");
+
+router.get("/", function (req, res, next) {
+  Tweet.find({}, (err, tweets) => {
+    if (err) res.send(err);
+    res.json(tweets);
+  });
+});
+
+router.get("/:id", (req, res, next) => {
+  Tweet.findOne({ _id: req.params.id }, (err, tweets) => {
+    if (err) res.send(err);
+    res.json(tweets);
+  });
+});
+
+router.post("/", (req, res, next) => {
+  let tweetInstance = new Tweet({
+    name: req.body.name,
+    proposals: [{ text: req.body.text }],
+  });
+  tweetInstance.save((err, tweet) => {
+    if (err) console.error(err);
+
+    res.json(tweet); // TODO: send proper response
+  });
+});
+
+router.post("/:id/proposal", (req, res, next) => {
+  console.log(req.body);
+  Tweet.findOneAndUpdate(
+    { _id: req.params.id },
+    { $push: { proposals: { text: req.body.text } } },
+    (err, tweets) => {
+      console.log(err);
+    }
+  );
+  res.json(); // TODO: send proper response
+});
+
+module.exports = router;
